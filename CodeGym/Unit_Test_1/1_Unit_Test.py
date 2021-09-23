4a15d5cd7e2638ed5d0078499848dffa7fe6a3af
Kedar Deo <kedar.s.deo75@gmail.com>
2021-09-23 17:45:36 +0530

    Add files via upload

---
+++ CodeGym/Unit_Test_1/1_Unit_Test.py
@@ -0,0 +1,13 @@
+#################################################
+#This python file contains
+#- Function under development
+#- Function to test
+#################################################
+
+
+def func(x):
+    return x + 1
+	
+def test_answer():
+    assert func(3) == 4
+    
